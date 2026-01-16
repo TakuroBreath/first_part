70851dc3077cfc8078d85a449dc01934ad8fa8db
Maksim Shokhirev <m.shokhirev@ll-games.com>
2026-01-16 20:04:17 +0300

    sdgsgsg

--- main.go
+++ main.go
@@ -1,15 +1,45 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
-	var a int = 10
+	s := []int{0, 1, 2, 3}
+	a := "sfsf"
+	m := map[int]int{1: 10, 2: 20, 3: 30}
 
-	fmt.Println("Woow!")
-	fmt.Println("So cool")
+	fmt.Println(s)
+	modifySlice(s)
+	fmt.Println(s)
 
-	fmt.Println(reflect.TypeOf(a))
+	println("")
+
+	fmt.Println(a)
+	modifyStr(a)
+	fmt.Println(a)
+
+	println("")
+
+	fmt.Println(m)
+	modifyMap(m)
+	fmt.Println(m)
+}
+
+func modifySlice(s []int) {
+	s[0] = 100
+}
+
+/*
+ * type Slice struct {
+ * 	pointer *[n]int
+ * len
+ * cap
+ * }
+ */
+
+func modifyStr(a string) {
+	a = "sfafsfdsgsgsdfsdfs"
+}
+
+func modifyMap(m map[int]int) {
+	m[1] = 12345
 }
