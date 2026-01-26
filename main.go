eefcda9800bbcdf2d448775a17648fd5b38702a4
Maksim Shokhirev <m.shokhirev@ll-games.com>
2026-01-26 15:56:34 +0300

    Интерфейсы

--- main.go
+++ main.go
@@ -2,45 +2,83 @@ package main
 
 import "fmt"
 
+type Repository interface {
+	Save()
+	Get()
+}
+
+type sqlite struct {
+	DSN string
+}
+
+func NewSqlite(dsn string) *sqlite {
+	return &sqlite{
+		DSN: dsn,
+	}
+}
+
+func (s sqlite) Save() {
+	fmt.Printf("Saved in sqlite in DSN: %v \n", s.DSN)
+}
+
+func (s sqlite) Get() {
+	fmt.Printf("Take your result from sqlite in DSN: %v \n", s.DSN)
+}
+
+type postgres struct {
+	Host string
+	Port string
+}
+
+func NewPostgres(host, port string) *postgres {
+	return &postgres{
+		Host: host,
+		Port: port,
+	}
+}
+
+func (p *postgres) Save() {
+	fmt.Printf("Save in postgres, host: %s, port: %s \n", p.Host, p.Port)
+}
+
+func (p *postgres) Get() {
+	fmt.Printf("Get from postgres, host: %s, port: %s, ENJOY IT! \n", p.Host, p.Port)
+}
+
+type Service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) *Service {
+	return &Service{
+		repo: repo,
+	}
+}
+
+func (s *Service) SaveAll() {
+	s.repo.Save()
+}
+
+func (s *Service) GetAll() {
+	s.repo.Get()
+}
+
+func Prnt(val any) {
+	switch v := val.(type) {
+	case int:
+		fmt.Print("Это инт! ", v)
+	case string:
+		fmt.Print("Это строка! ", v)
+	default:
+		fmt.Print("Я хз че эт:(")
+	}
+}
+
 func main() {
-	s := []int{0, 1, 2, 3}
-	a := "sfsf"
-	m := map[int]int{1: 10, 2: 20, 3: 30}
-
-	fmt.Println(s)
-	modifySlice(s)
-	fmt.Println(s)
-
-	println("")
-
-	fmt.Println(a)
-	modifyStr(a)
-	fmt.Println(a)
-
-	println("")
-
-	fmt.Println(m)
-	modifyMap(m)
-	fmt.Println(m)
-}
-
-func modifySlice(s []int) {
-	s[0] = 100
-}
-
-/*
- * type Slice struct {
- * 	pointer *[n]int
- * len
- * cap
- * }
- */
-
-func modifyStr(a string) {
-	a = "sfafsfdsgsgsdfsdfs"
-	fmt.Println(a)
-}
-
-func modifyMap(m map[int]int) {
-	m[1] = 12345
+	// pg := NewPostgres("localhost", "5432")
+	// s := NewService(pg)
+	// s.GetAll()
+	// s.SaveAll()
+	// 
+	Prnt(3i+1)
 }
