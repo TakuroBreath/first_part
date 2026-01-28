1345e6dea2ac6907d188de1aee5f643bc546bd29
Maksim Shokhirev <m.shokhirev@ll-games.com>
2026-01-28 18:15:12 +0300

    Задача на интерфейсы

--- main.go
+++ main.go
@@ -1,29 +1,63 @@
+/*
+* Задача: «Система уведомлений»
+Контекст:
+Вы разрабатываете простую систему уведомлений. Система должна уметь отправлять сообщения через разные каналы (Email, SMS).
+Техническое задание:
+Интерфейс: Создайте интерфейс Notifier с методом Send(message string).
+Структуры:
+Создайте структуру EmailNotifier с полем EmailAddress.
+Создайте структуру SMSNotifier с полем PhoneNumber.
+Методы: Реализуйте метод Send для каждой структуры:
+EmailNotifier.Send должен выводить в консоль: "Отправка Email на [адрес]: [сообщение]"
+SMSNotifier.Send должен выводить в консоль: "Отправка SMS на номер [номер]: [сообщение]"
+Основная функция (main):
+Создайте слайс (slice) типа []Notifier.
+Добавьте в него один экземпляр EmailNotifier и один SMSNotifier.
+В цикле пройдитесь по слайсу и отправьте сообщение "Hello Go!" через каждый нотификатор.
+Ожидаемый вывод:
+Отправка Email на user@example.com: Hello Go!
+Отправка SMS на номер +79990000000: Hello Go!
+*/
 package main
 
 import "fmt"
 
-type SecretString struct {
-	secret string
-}
-
-func NewSecretString(s string) SecretString {
-	return SecretString{
-		secret: s,
+type (
+	Notifier interface {
+		Send(message string)
 	}
+
+	EmailNotifier struct {
+		EmailAddress string
+	}
+
+	SMSNotifier struct {
+		PhoneNumber string
+	}
+)
+
+func (e EmailNotifier) Send(message string) {
+	fmt.Printf("Отправка на Email на %s: %s\n", e.EmailAddress, message)
 }
 
-func (s SecretString) Value() string {
-	return s.secret
-}
-
-func (s SecretString) String() string {
-	return "******"
+func (s SMSNotifier) Send(message string) {
+	fmt.Printf("Отправка на SMS на %s: %s\n", s.PhoneNumber, message)
 }
 
 func main() {
-	value := "sensetive data"
-	
-	s := NewSecretString(value)
-	
-	fmt.Print(s)
+	s := []Notifier{}
+
+	e := EmailNotifier{
+		EmailAddress: "user@example.com",
+	}
+
+	sn := SMSNotifier{
+		PhoneNumber: "+79990000000",
+	}
+
+	s = append(s, e, sn)
+
+	for _, v := range s {
+		v.Send("Hello Go!")
+	}
 }
